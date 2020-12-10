2f3e178e3a2ec2ac96b2ecb85404eacbd4a291ac
Abdul Latif <alatif.wijaya@gmail.com>
2020-12-10 13:27:47 +0700

    Updates

---
+++ precache-manifest.7c42cffb3df657da821b83e2887966c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397e633e93d1590bf0ddacd83b1f8285",
+    "url": "/index.html"
+  },
+  {
+    "revision": "919205580bc2b0136544",
+    "url": "/static/css/main.e10cfd29.chunk.css"
+  },
+  {
+    "revision": "7bad87bdedbcdb05f1ba",
+    "url": "/static/js/2.c64ad676.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.c64ad676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919205580bc2b0136544",
+    "url": "/static/js/main.9887a4a6.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  },
+  {
+    "revision": "5b82755940079eb6d503c50f8b2bd8f9",
+    "url": "/static/media/CV.5b827559.pdf"
+  },
+  {
+    "revision": "0b6286c2d9dae6bb848faf51a2007eb2",
+    "url": "/static/media/Capture.0b6286c2.PNG"
+  },
+  {
+    "revision": "1592b21134c24bf9f9d99091f775585d",
+    "url": "/static/media/sosmedcollection.1592b211.jpg"
+  }
+]);
